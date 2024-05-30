1d3ba0460001b62b4e3e0acbc585092416018a4e
Tallal Mohar <tallalmoohar@gmail.com>
2024-05-30 15:50:05 -0700

    games finished!

--- scripts.js
+++ scripts.js
@@ -6,8 +6,6 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-let player1Num = getRandomInt(6);
-let player2Num = getRandomInt(6);
 
 //initializing and putting each img in an array 
 let diceImg1 = "<img class='player1-img' src='./images/dice1.png'>"
@@ -20,12 +18,32 @@ let diceImg6  = "<img class='player1-img' src='./images/dice6.png'>"
 let imgArray = [diceImg1,diceImg2,diceImg3,diceImg3,diceImg4,diceImg5,diceImg6]
 
 //testing
-let randomImgSelected1 = imgArray[getRandomInt(6)];
-let randomImgSelected2 = imgArray[getRandomInt(6)];
+
 let playerImg1 = document.querySelector(".player1-div");
 let playerImg2 = document.querySelector(".player2-div");
 
-playerImg1.innerHTML = randomImgSelected1;
-playerImg2.innerHTML = randomImgSelected2;
+//get the button and change the imgs with each click
+let playButton = document.querySelector(".play-btn");
+
+
+playButton.addEventListener("click",function(){
+    let player1Num = getRandomInt(6);
+    let player2Num = getRandomInt(6);
+
+    let randomImgSelected1 = imgArray[player1Num];
+    let randomImgSelected2 = imgArray[player2Num];
+    playerImg1.innerHTML = randomImgSelected1;
+    playerImg2.innerHTML = randomImgSelected2;
+    let heading = document.querySelector(".main-header");
+
+    //changes the H1 depding on who wins
+    if(player1Num > player2Num){
+        heading.innerHTML = "<h1 class='main-header'>🚩Player 1 Wins!</h1>"
+    }else{
+        heading.innerHTML = "<h1 class='main-header'>🏳️Player 2 Wins!</h1>"
+    }
+
+    });
+    
 
 
